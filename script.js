19ccd45fc070bf3d030a5af59fd22089893a7d40
Aom <p.sithanant@gmail.com>
2017-01-19 23:18:47 -0800

    checkerboard red/black completed

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 // Your JS goes here
+document.addEventListener("DOMContentLoaded", function(){
+  // Handler when the DOM is fully loaded
+  var body = document.querySelector('body');
+
+  for (var i = 0; i < 108; i++) {
+    var div = document.createElement('div');
+    body.appendChild(div);
+    if (i % 2 === 1) {
+      div.style["background-color"] = "black";
+    } else {
+      div.style["background-color"] = "red";
+    }
+    div.style.width = "11.1%";
+    div.style.float = "left";
+    div.style.paddingBottom = '11.1%';
+  }
+});
+
+// Each tile should be a `div`
+// * Each tile's width is `11.1%`
+// * Set each tile's `float` property to `left`
+// * Each tile's paddingBottom is `11.1%`
